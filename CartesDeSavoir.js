7853df7622a0c8d2d94254f42b650be3fbc8f3dd
Samuel <Samuel>
2023-12-08 01:39:13 +0100

    Ajout dun halo

--- CartesDeSavoir.js
+++ CartesDeSavoir.js
@@ -81,22 +81,25 @@ class CartesDeSavoir extends HTMLElement
             image.setAttribute("src", this._pathImage);
             divcontainerCarte.appendChild(image);
 
-            let choixCorrection = document.createElement("h3");
-            choixCorrection.className = "choixCorrection";
-            if (this._choixCorrection == true) {
-                choixCorrection.textContent = "✅";
-            } else {
-                choixCorrection.textContent = "❌";
-            }
-            divcontainerCarte.appendChild(choixCorrection);
-
             let divcontainerTexte = document.createElement("div");
             divcontainerTexte.className = "texte";
             divcontainerCarte.appendChild(divcontainerTexte);
 
-                let titre = document.createElement("h2");
-                titre.textContent = this._titre;
-                divcontainerTexte.appendChild(titre);
+                let containerTitre = document.createElement("div");
+                containerTitre.className = "containerTitre";
+                divcontainerTexte.appendChild(containerTitre);
+                    let titre = document.createElement("h2");
+                    titre.textContent = this._titre;
+                    containerTitre.appendChild(titre);
+                
+                    let choixCorrection = document.createElement("h3");
+                    choixCorrection.className = "choixCorrection";
+                    containerTitre.appendChild(choixCorrection);
+                    if (this._choixCorrection == true) {
+                        choixCorrection.textContent = "✅";
+                    } else {
+                        choixCorrection.textContent = "❌";
+                    }
 
                 let veracite = document.createElement("h3");
                 veracite.className = "veracite";
@@ -115,61 +118,6 @@ class CartesDeSavoir extends HTMLElement
         this.addEventListener('click', () => {
             window.location.href = this._lien;
         });
-                
-
-        /*
-        // Création des éléments
-        let image = document.createElement("img");
-        let titre = document.createElement("h3");
-        let description = document.createElement("p");
-        let lien = document.createElement("a");
-        let veracite = document.createElement("p");
-        let choixCorrection = document.createElement("p");
-
-        // Ajout des classes
-        image.classList.add("image");
-        titre.classList.add("titre");
-        description.classList.add("description");
-        lien.classList.add("lien");
-        veracite.classList.add("veracite");
-        choixCorrection.classList.add("choix");
-
-        // Ajout des attributs
-        image.setAttribute("src", this._pathImage);
-        lien.setAttribute("href", this._lien);
-
-        // Ajout des éléments au DOM
-        this.appendChild(image);
-        this.appendChild(titre);
-        this.appendChild(description);
-        this.appendChild(lien);
-        this.appendChild(veracite);
-        this.appendChild(choixCorrection);
-
-        // Ajout du texte aux éléments
-        titre.textContent = this._titre;
-        description.textContent = this._description;
-
-        if (this._veracite == true) {
-            veracite.textContent = "VRAI";
-        } else {
-            veracite.textContent = "FAUX";
-        }
-
-        if (this._choixCorrection == true) {
-            choixCorrection.textContent = "✅";
-        } else {
-            choixCorrection.textContent = "❌";
-        }
-
-        // Ajout de l'événement
-        this.addEventListener('click', () => {
-            window.location.href = this._lien;
-        });
-
-        // Ajout de la carte au DOM
-        //document.getElementById("cartes").appendChild(this);
-        */
     }
 
 } window.customElements.define("carte-savoir", CartesDeSavoir);
